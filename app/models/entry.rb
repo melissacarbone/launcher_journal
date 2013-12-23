12c485abe297a9fe2a68d34dc38f6ec6d73b3a47
Melissa Carbone <mcarbone1@gmail.com>
2013-12-23 13:46:06 -0500

    add category model and tests

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,4 +2,6 @@ class Entry < ActiveRecord::Base
   validates :title, :format => { :with => /[a-zA-Z]+/ }
   validates :description, :format => { :with => /[a-zA-Z]+/ }
 
+  belongs_to :category
+
 end
--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -7,4 +7,7 @@ describe Entry do
   it { should have_valid(:description).when('This is my entry with some numbers 123') }
   it { should_not have_valid(:description).when(' ', nil, 123) }
 
+  it { should belong_to(:category) }
+  #it { should belong_to(:user) }
+
 end
